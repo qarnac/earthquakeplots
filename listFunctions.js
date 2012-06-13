1d9cadfc58540ee40087672e1ed31b4890e24462
Jeffrey Rackauckas <Jeffreyrack@gmail.com>
2012-06-13 10:19:57 -0700

    Made earthquakes selectable.

--- listFunctions.js
+++ listFunctions.js
@@ -6,24 +6,32 @@ var selectedEarthquake=0;
 
 function changeSelectedEarthquake(value){
     selectedEarthquake=value
-    alert("Function has been called.");
+	showSelectedEarthquake();
 }
 
 // Is called once the csv has been retrieved and parsed.  Simply takes the top 5 earthquakes, and displays them in a table.
 function updateList(newList){
     var table=document.getElementById("earthquakeTable");
     for (var i=1; i<6; i++){
-	var parent=document.createElement("tr");
-	childA=document.createElement("td");
-	childB=document.createElement("td");
-	// Temporary quotation mark fix.
-	newList[i][11]=newList[i][11].replace('&quot;', "");
-	newList[i][11]=newList[i][11].replace('&quot;', "");
+		var parent=document.createElement("tr");
+		childA=document.createElement("td");
+		childB=document.createElement("td");
+		// Temporary quotation mark fix.
+		newList[i][11]=newList[i][11].replace('&quot;', "");
+		newList[i][11]=newList[i][11].replace('&quot;', "");
 	
-	childA.textContent=newList[i][11];
-	childB.textContent=newList[i][8];
-	parent.appendChild(childA);
-	parent.appendChild(childB);
-	table.appendChild(parent);
+		childA.textContent=newList[i][11];
+		childB.textContent=newList[i][8];
+		parent.appendChild(childA);
+		parent.appendChild(childB);
+		parent.setAttribute("onclick", "changeSelectedEarthquake(" + i + ")");
+		table.appendChild(parent);
+	
 	}
+}
+
+function showSelectedEarthquake(){
+	parent=document.createElement("p");
+	parent.textContent=selectedEarthquake;
+	document.getElementById("list").appendChild(parent);
 }
